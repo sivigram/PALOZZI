248e3ebca0407518dc2b8a9672050f6ec859ed8f
sivigram <fabiosiviero97@gmail.com>
2026-08-05 11:18:38 +1000

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,10 @@
-import React from 'react';import { createRoot } from 'react-dom/client';import App from './App';import './styles/global.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
