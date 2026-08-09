9e70205e9e574b29a24a2e64eadee049eb0d6fac
sivigram <fabiosiviero97@gmail.com>
2026-08-10 09:56:08 +1000

    Add files via upload

--- src/components/ColourCard.tsx
+++ src/components/ColourCard.tsx
@@ -1,2 +1,30 @@
-import { PaletteColour } from '../types/colourAnalysis';import { bestTextColour, formatHsl, formatRgb, pantoneLabel } from '../utils/colourConversions';
-export function ColourCard({colour,showTechnical}:{colour:PaletteColour;showTechnical:boolean}){return <article className="colour-card"><div className="swatch" style={{backgroundColor:colour.hex,color:bestTextColour(colour.hex)}}>{colour.category}</div><div className="colour-info"><strong>{colour.name}</strong><span>{colour.hex}</span>{showTechnical&&<><span>{pantoneLabel(colour.pantone,colour.pantoneStatus)}</span><span>{formatRgb(colour.hex)}</span><span>{formatHsl(colour.hex)}</span></>}</div></article>}
+import { PaletteColour } from '../types/colourAnalysis';
+import { bestTextColour, formatHsl, formatRgb, pantoneLabel } from '../utils/colourConversions';
+
+type Props = {
+  colour: PaletteColour;
+  showTechnical: boolean;
+};
+
+export function ColourCard({ colour, showTechnical }: Props) {
+  return (
+    <article className="colour-card">
+      <div
+        className="swatch"
+        style={{ backgroundColor: colour.hex, color: bestTextColour(colour.hex) }}
+      >
+        <strong className="swatch-name">{colour.name}</strong>
+      </div>
+      <div className="colour-info">
+        <span>{colour.hex}</span>
+        {showTechnical && (
+          <>
+            <span>{pantoneLabel(colour.pantone, colour.pantoneStatus)}</span>
+            <span>{formatRgb(colour.hex)}</span>
+            <span>{formatHsl(colour.hex)}</span>
+          </>
+        )}
+      </div>
+    </article>
+  );
+}
